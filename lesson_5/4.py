41bdfc2fdccbf14c445494b4d0cf5333a5132b6c
annadorzhieva <luxoria@bk.ru>
2020-12-13 13:43:46 +0300

    Add lesson 5

---
+++ lesson_5/4.py
@@ -0,0 +1,23 @@
+'''
+4. Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
+'''
+
+numbers = 'One — 1\nTwo — 2\nThree — 3\nFour — 4'
+
+with open('numbers.txt', 'w', encoding='utf-8') as f:
+    f.write(numbers)
+
+ru = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре'}
+
+new_numbers = []
+with open('numbers.txt', 'r') as f:
+    for el in f:
+        el = el.split(' ', 1)
+        new_numbers.append(ru[el[0]] + el[1])
+    print(new_numbers)
